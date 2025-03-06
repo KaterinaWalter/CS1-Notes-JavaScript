e5a1d6dd3e545082824c00e792d9ca4861c80aac
Katerina Walter <94501766+katerinanavab@users.noreply.github.com>
2025-03-06 15:41:09 +0000

    4.2 notes

--- script.js
+++ script.js
@@ -32,3 +32,28 @@ let worstNumber = 6;
 // You can "PASS IN" a VARIABLE into a FUNCTION 
 console.log(luckyNumber);
 console.log(worstNumber); 
+
+// Can CHANGE the value stored in a variable
+let mood = "Happy";
+console.log(mood);
+mood = "Sad";
+console.log(mood);
+
+// Can COPY the value from one variable into another
+let isHappy = true;
+let isThrilled;
+isThrilled = isHappy;
+console.log(isThrilled);
+
+// MATH OPERATIONS
+let score = 0;
+score = score + 5;
+console.log(score); 
+score = score - 10;
+console.log(score); 
+score = score * (-3);
+console.log(score);
+score = score / 5;
+console.log(score);
+
+
